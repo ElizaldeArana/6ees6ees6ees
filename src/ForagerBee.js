3845975cb905e460520e18990691fe85e1a07995
Javier Elizalde <javelisol@gmail.com>
2018-01-22 16:27:53 -0600

    HoneyMaker has been completed

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 class ForagerBee {
-  // TODO..
+  constructor() {
+    this.age = 10;
+    this.color = "yellow";
+    this.food = "jelly";
+    this.job = "find pollen"
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++
+  }
+  giveHoney() {
+    this.honeyPot--
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly'
+
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 class HoneyMakerBee {
-  // TODO..
+  constructor() {
+    this.age = 10;
+    this.color = "yellow";
+    this.food = "jelly";
+    this.job = "make honey"
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot++
+  }
+  giveHoney() {
+    this.honeyPot--
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly'
+
+  }
 };
