5f26c44fdbb49babc653c88acf7dd12fff5fe525
Javier Elizalde <javelisol@gmail.com>
2018-01-22 16:40:38 -0600

    RetiredForager has been completed

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,21 @@
 class RetiredForagerBee {
-  // TODO..
-};
+  constructor() {
+    this.age = 40;
+    this.color = "grey";
+    this.food = "jelly";
+    this.job = "gamble"
+    this.honeyPot = 0;
+    this.canFly = false;
+    this.treasureChest = [];
+  }
+  forage() {
+    return ("I am too old, let me play cards instead");
+  }
+  gamble(treasure) {
+    this.treasureChest.push(treasure)
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly'
+
+  }
+}
